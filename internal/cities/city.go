5a9444fcd23a9086f06a14e386a3957c63accefa
FranciscoCardo09 <pnchocardo@gmail.com>
2026-02-17 15:43:40 -0300

    agregar modelo de ciudad y datos

---
+++ internal/cities/city.go
@@ -0,0 +1,9 @@
+package cities
+
+// City representa una ciudad disponible para consultar clima.
+type City struct {
+	ID        string  `json:"id"`
+	Name      string  `json:"name"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
